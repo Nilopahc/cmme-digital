ee96fd1e7db7f30d7d3588a069012bcf2aa197a1
Adiel Emilson <adielenilson@gmail.com>
2025-02-16 23:49:27 -0300

    removendo seletor de cidade por input radio

--- js/cadastro.js
+++ js/cadastro.js
@@ -13,29 +13,3 @@ function submit(){
         alert("Não deixe espaços em brancos")
     }
 }
-/*function city(){
-    let city = document.getElementsByName('city')
-    let res = document.getElementById('res')
-    let citys = document.querySelector('.city')
-    let prrg = document.getElementById('prrg')
-    if(city[0].checked){
-        if(!prrg){
-            let rua = document.createElement('input')
-            let prg = document.createElement('p')
-            let label = document.createElement('label')
-
-            label.setAttribute('for', 'rua')
-            label.textContent = 'Rua'
-            rua.setAttribute('type', 'text')
-            rua.setAttribute('id', 'rua')
-            prg.setAttribute('id', 'prrg')
-
-            prg.appendChild(label)
-            prg.appendChild(rua)
-            citys.appendChild(prg)
-        }
-    }else{
-        prrg.remove()
-        citys.appendChild('')
-    }
-}*/
